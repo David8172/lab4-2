70507c9e7f8ee3016c98ac0c377c2c54cf518384
DavyCheng <chw_email@163.com>
2024-03-19 21:13:34 +0800

    1st commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+﻿#include <QCoreApplication>
+#include <QDebug>
+#include "bubblesort.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    srand(time(0));
+
+    QVector<int> vec(10);
+    for(int i = 0 ;i<10;i++)
+    {
+        vec[i] = rand() % 100;
+    }
+    qDebug() << "原始数列 ：" << vec;
+
+    BubbleSort(vec);
+//    std::sort(vec.begin(),vec.end());
+
+    qDebug() << "已排序数列 ：" << vec;
+
+    return a.exec();
+}
